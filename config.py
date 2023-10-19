193eab6ed22690a79aeeb4f6b7b54e62a1713f3b
MelisaMC <melisa.mc89@gmail.com>
2023-10-19 14:20:42 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+local_path = '/home/melma31/Documents/single_cell_sequencing'
+data_path = os.path.join(local_path, 'data')
+figures_path = os.path.join(local_path, 'figures')
+kallisto_path = os.path.join(local_path, 'src')
+output_path = os.path.join(local_path, 'output')
